9adb15e7309ad36b172cbc7859a32307a7f30a12
acquijano <alvin.quijano@live.ca>
2017-07-13 18:05:13 -0400

    Instantiated two objects, one SuperHero and one SuperVillain

--- comp123 - week09 lesson a/IHasMalice.cs
+++ comp123 - week09 lesson a/IHasMalice.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/*
+ * Name: Alvin Quijano
+ * Date: July 13, 2017
+ * Description: This is the Malice interface
+ * Version: 0.1 - created the IHasMalice interface for the SuperVillain class
+ */
+
 namespace comp123___week09_lesson_a
 {
     public interface IHasMalice
     {
+        // PUBLIC PROPERTIES 
+        int Malice { get; set; }
     }
 }
--- comp123 - week09 lesson a/Program.cs
+++ comp123 - week09 lesson a/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Alvin Quijano
  * Date: July 11, 2017
  * Description: This is the driver class for my project
- * Version: 0.1 - instantiated an object of typpe Super Human
+ * Version: 0.2 - instantiated two objects of type SuperHero and SuperVillain
  */
 
 namespace comp123___week09_lesson_a
@@ -20,7 +20,17 @@ namespace comp123___week09_lesson_a
             SuperHuman superhuman = new SuperHuman("super dude");
             superhuman.AddPower("Spider Climbing", 50);
             superhuman.AddPower("Webcasting", 40);
-            Console.WriteLine(superhuman.ToString());          
+            Console.WriteLine(superhuman.ToString());
+
+
+            SuperHero hero1 = new SuperHero("Captain America", 100);
+            hero1.AddPower("Shield", 40);
+            hero1.AddPower("Super Strength", 30);
+            Console.WriteLine(hero1.ToString());
+
+            SuperVillain villain1 = new SuperVillain("Thanos", 9999);
+            villain1.AddPower("Infinity Gauntlet", 999999999);
+            Console.WriteLine(villain1.ToString());    
         }
     }
 }
